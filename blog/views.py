67ceac397cf932832990bde5b676d8c881b735ee
eunyoung <kwkdmsdud@naver.com>
2022-11-21 12:43:51 +0900

    CBV로 블로그 포스트 목록 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,34 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+    #template_name = 'blog/post_list.html'
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
+class PostDetail(DetailView):
+    model = Post
 
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts' : posts,
-        }
-    )
+# FBV 방식 주석처리
+#def index(request):
+#    posts = Post.objects.all().order_by('-pk')
+#
+#    return render(
+#        request,
+#        'blog/post_list.html',
+#        {
+#            'posts' : posts,
+#        }
+#    )
 
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
 
     return render(
         request,
-        'blog/single_post_page.html',
+        'blog/post_detail.html',
         {
             'post' : post,
         }
