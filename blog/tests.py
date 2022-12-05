398eafb2d5503f576496ef5c838a6f897bbd4264
eunyoung <kwkdmsdud@naver.com>
2022-12-05 14:55:21 +0900

    포스트 작성 페이지 접근 권한 설정하기

--- blog/tests.py
+++ blog/tests.py
@@ -10,6 +10,9 @@ class TestView(TestCase) :
         self.user_trump = User.objects.create_user(username='trump', password='somepassword')
         self.user_obama = User.objects.create_user(username='obama', password='somepassword')
 
+        self.user_obama.is_staff = True
+        self.user_obama.save()
+
         # 카테고리
         self.category_programming = Category.objects.create(name='programming', slug='programming')
         self.category_music = Category.objects.create(name='music', slug='music')
@@ -194,9 +197,13 @@ class TestView(TestCase) :
         response = self.client.get('/blog/create_post/')
         self.assertNotEqual(response.status_code, 200)
 
-        # 로그인을 한다.
+        # staff가 아닌 trump가 로그인을 한다.
         self.client.login(username='trump', password='somepassword')
+        response = self.client.get('/blog/create_post/')
+        self.assertNotEqual(response.status_code, 200)
 
+        # staff인 obama로 로그인 한다.
+        self.client.login(username='obama',password='somepassword')
         response = self.client.get('/blog/create_post/')
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
