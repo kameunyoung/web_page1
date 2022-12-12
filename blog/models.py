6d865b4448207dd66d5e2fce02532324ffde470c
eunyoung <kwkdmsdud@naver.com>
2022-12-13 08:48:34 +0900

    사용자 아바타 보여주기

--- blog/models.py
+++ blog/models.py
@@ -73,3 +73,9 @@ class Comment(models.Model):
 
     def get_absolute_url(self):
         return f'{self.post.get_absolute_url()}#comment-{self.pk}'
+
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return 'https://cdn.dribbble.com/users/2321513/avatars/normal/9e842dd23c3ad5cbe4115ece3483f8d0.png?1641883190&compress=1&resize=50x50'
